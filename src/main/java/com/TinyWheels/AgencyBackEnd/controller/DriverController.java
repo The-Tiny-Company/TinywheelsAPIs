bc8f964fbad9ec93ba906cb63349b2d64dbfc790
Chbaly Adil <adil-chbaly100@hotmail.com>
2023-03-28 22:59:19 +0000

    Ticket -> BackEndAgency : Fixed and Issue with Cross Origin

--- src/main/java/com/TinyWheels/AgencyBackEnd/controller/DriverController.java
+++ src/main/java/com/TinyWheels/AgencyBackEnd/controller/DriverController.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.TinyWheels.AgencyBackEnd.model.Driver;
 import com.TinyWheels.AgencyBackEnd.repository.DriverRepository;
 
+
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1")
 public class DriverController {
--- src/main/java/com/TinyWheels/AgencyBackEnd/controller/VehiculeController.java
+++ src/main/java/com/TinyWheels/AgencyBackEnd/controller/VehiculeController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.TinyWheels.AgencyBackEnd.model.Vehicule;
 import com.TinyWheels.AgencyBackEnd.repository.VehiculeRepository;
 
+
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("api/v1")
 public class VehiculeController {
